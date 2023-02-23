aabb50d7d15b69d9598e025c54332d110d6eff8f
Gustavo <gustavo27hs@gmail.com>
2023-02-23 20:29:25 -0300

    Desafio do módulo de vetores. Insere o nome e email do aluno que pretende alugar o quarto, após isso, ocupa a posição no vetor conforme o número do quarto

---
+++ DesafioVetor/src/Entities/Student.java
@@ -0,0 +1,27 @@
+package Entities;
+
+public class Student {
+    private String name;
+    private String email;
+
+    private int room;
+
+
+    public Student(String name, String email, int room) {
+        this.name = name;
+        this.email = email;
+        this.room = room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+}
---
+++ DesafioVetor/src/Main.java
@@ -0,0 +1,50 @@
+
+
+import Entities.Student;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("How many rooms will be rented: ");
+
+        int n = sc.nextInt();
+        Student[] student = new Student[9];
+
+
+        for (int i = 0; i < n; i++) {
+
+            sc.nextLine();
+            System.out.printf("Rent %d%n", i + 1);
+            System.out.printf("Name: ");
+            String name = sc.nextLine();
+
+            System.out.printf("Email: ");
+            String email = sc.nextLine();
+
+            System.out.printf("Room: ");
+            int room = sc.nextInt();
+
+            student[room] = new Student(name, email, room);
+
+
+        }
+
+        for (int i = 0; i < student.length; i++) {
+
+            if (student[i] != null) {
+
+                System.out.println(student[i].getRoom() + ": " + student[i].getName() + ", " + student[i].getEmail());
+
+            }
+        }
+
+
+    }
+}
